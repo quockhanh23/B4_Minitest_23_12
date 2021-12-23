240078302d7b3b7770ae45ae904ca4a4c27bc10e
unknown <yeuthuongvoivangdiqua@gmail.com>
2021-12-23 11:00:27 +0700

    fix somthing

--- src/main/java/repository/StudentRepository.java
+++ src/main/java/repository/StudentRepository.java
@@ -1,6 +1,7 @@
 package repository;
 
 import model.Student;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
